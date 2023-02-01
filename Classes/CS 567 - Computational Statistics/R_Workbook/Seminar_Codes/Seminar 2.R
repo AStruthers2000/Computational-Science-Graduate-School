e5ed9bf55b58200385364092084fc92fcc704222
Andrew Struthers <struthers919@gmail.com>
2023-02-01 12:23:05 -0800

    Started working on seminar 2

---
+++ Classes/CS 567 - Computational Statistics/R_Workbook/Seminar_Codes/Seminar 2.R
@@ -0,0 +1,16 @@
+#Install and load necessary libraries
+install.packages("Hmisc")
+install.packages("polycor")
+install.packages("ggm")
+install.packages("Rcmdr")
+
+library(Hmisc)
+library(ggplot2)
+library(boot)
+library(polycor)
+library(ggm)
+library(Rcmdr)
+
+#Read the data from file
+setwd("C://Users//Strut//DocumentsSchool//Grad School//Classes//CS 567 - Computational Statistics//")
+getwd()
