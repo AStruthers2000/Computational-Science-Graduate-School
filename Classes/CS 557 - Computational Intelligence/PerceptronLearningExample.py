b9066c4f197acb2140a0a4cada19bb8531b35e3a
Andrew Struthers <struthers919@gmail.com>
2023-04-13 17:44:57 -0700

    Tweaked Perceptron and added CRC for networks

--- Classes/CS 557 - Computational Intelligence/PerceptronLearningExample.py
+++ Classes/CS 557 - Computational Intelligence/PerceptronLearningExample.py
@@ -8,6 +8,9 @@ Created on Mon Apr 10 16:33:20 2023
              included here.
 """
 
+from random import randint
+import math
+
 def dot(x, y):
     assert len(x) == len(y)
     return sum([x[i] * y[i] for i in range(len(w))])
@@ -15,29 +18,41 @@ def dot(x, y):
 def scalar_multiply(a, s):
     return [x * s for x in a]
 
-inputs = [[2, 1, -1],
-          [0, -1, -1]]
+inputs = [[2, 1, 0, 4, 1, 39, 2, -1],
+          [0, -1, 8, 9, -1, 2, 10, -1],
+          [1, -1, 4, 5, 3, 2, -2, -1],
+          [0, -1, 3, 4, -1, 2, 1, -1]]
 
-w = [0, 1, 0]
+w = [randint(-1, 1) for _ in range(len(inputs[0]))]
 
-t = [-1, 1]
+t = [-1, 1, 1, -1]
 
 o = [999 for i in range(len(t))]
 
-n = 1
+n = 0.01
 
 cases_passed = 0
 
 while t != o:
+    print(f"Current weights: {w}")
+    print(f"Current outputs: {o}")
+    print(f"Desired outputs: {t}")
+    print("=========="*5)
     for i in range(len(inputs)):
         x = inputs[i]
         net = dot(w, x)
 
         act = 1 if net > 0 else -1
         if act != t[i]:
-            print("correcting weights")
             error = scalar_multiply(x, n*(t[i] - net))
             w = [w[i] + error[i] for i in range(len(error))]
-        else:
-            o[i] = act
-        print(w)
+        o[i] = act
+    if sum([1 if math.isnan(w[i]) else 0 for i in range(len(w))]) > 0:
+        print("Detected NaN")
+        break
+
+print("Final weights:   {0}".format(w))
+print("Final outputs:   {0}".format(o))
+print("Desired outputs: {0}".format(t))
+print("=========="*5)
+        
